1b1ee87e1e953e99d6ad3429fb22f772e5e17d32
THANGAPERUMALE <149352990+THANGAPERUMALE@users.noreply.github.com>
2023-10-31 10:38:45 +0530

    Create Voting.java

---
+++ Voting.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int age=sc.nextInt();
+    if(age<18){
+        System.out.println("Not eligible");
+    }
+    else{
+        System.out.println("Eligible");
+    } 
+  }
+}
