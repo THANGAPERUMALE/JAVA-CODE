8e595271723e452f566491b30a832a733d39d64e
THANGAPERUMALE <149352990+THANGAPERUMALE@users.noreply.github.com>
2023-12-14 11:19:34 +0530

    Update ArrayAddition.java

--- arrAddition/ArrayAddition.java
+++ arrAddition/ArrayAddition.java
@@ -1,15 +1,15 @@
 package com.arrayAddition;
 
-public class ArrayPassing {
+public class ArrayAddition {
 	public static void add(int[]a) {
 		int sum=0;
 		for(int i:a) {
 		  sum+=i;
 		}
-		System.out.print("Sum of Elements = "+sum);
+		System.out.print("Sum = "+sum);
 	}
 	public static void main(String[]args) {
-		int arr[]= {10,27,37,7};
+		int arr[]= {20,56,87,9};
 		add(arr);
 	}
 }
