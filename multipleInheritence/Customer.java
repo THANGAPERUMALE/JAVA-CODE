1e981ad6b6b91f8a539b4f30efb7440d609bab04
THANGAPERUMALE <149352990+THANGAPERUMALE@users.noreply.github.com>
2023-12-14 11:44:44 +0530

    Update Customer.java

--- multipleInheritence/Customer.java
+++ multipleInheritence/Customer.java
@@ -1,17 +1,17 @@
-package com.MultipleInheritence;
+package com.multipleInheritence;
 
 public class Customer implements Car,Bike{
 	public void speedOfBike() {
-		System.out.println("Maximum Speed of Bike is 120");
+		System.out.println("Maximum Speed of Bike is 160");
 	}
 	public void mileageOfBike() {
-		System.out.println("MileAge Of This Bike is 40/L");
+		System.out.println("MileAge Of This Bike is 45/L");
 	}
 	public void speedOfCar() {
-		System.out.println("Maximum Speed of Car is 200");
+		System.out.println("Maximum Speed of Car is 180");
 	}
 	public void mileageOfCar() {
-		System.out.println("MileAge Of This Car is 35/L");
+		System.out.println("MileAge Of This Car is 30/L");
 	}
 	public static void main(String[] args) {
 		Customer c=new Customer();
