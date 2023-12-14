405964c762180711092663747ff4de343bb200c7
THANGAPERUMALE <149352990+THANGAPERUMALE@users.noreply.github.com>
2023-12-14 11:37:08 +0530

    Update Main.java

--- encaps/Main.java
+++ encaps/Main.java
@@ -1,13 +1,13 @@
-package com.encapsulation;
+package com.encaps;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Employee d=new Employee();
-		d.name="Pradeep";
-		d.deptName="Developer";
+		d.name="Thangaperumal";
+		d.deptName="EEE";
 		d.location="Chennai";
-		d.setId(1117);
+		d.setId(8011);
 		System.out.println(d.name);
 		System.out.println(d.deptName);
 		System.out.println(d.location);
