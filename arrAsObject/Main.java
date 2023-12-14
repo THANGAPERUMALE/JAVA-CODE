b9c5889faaa5993ca41e58aa9dd998da4f92b920
THANGAPERUMALE <149352990+THANGAPERUMALE@users.noreply.github.com>
2023-12-14 11:13:49 +0530

    Update Main.java

--- arrAsObject/Main.java
+++ arrAsObject/Main.java
@@ -1,12 +1,12 @@
-package com.arrayAsObject;
+package com.arrAsObject;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Employee employee[]=new Employee[3];
-		employee[0]=new Employee(1001,"abc","kerala");
-		employee[1]=new Employee(1002,"pqr","bengalore");
-		employee[2]=new Employee(1003,"suv","chennai");
+		employee[0]=new Employee(1,"abc","kerala");
+		employee[1]=new Employee(2,"pqr","mumbai");
+		employee[2]=new Employee(3,"suv","chennai");
 		for(Employee i:employee) {
 			System.out.println(i.id+" "+i.name+" "+i.location);
 		}
