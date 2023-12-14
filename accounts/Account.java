4c6a717cea8811db0129fc5e9e1c8f21df8a7a80
THANGAPERUMALE <149352990+THANGAPERUMALE@users.noreply.github.com>
2023-12-14 12:05:43 +0530

    Add files via upload

---
+++ accounts/Account.java
@@ -0,0 +1,24 @@
+package com.accout;
+
+public class Account {
+	private int accno;
+	private String ifsccode;
+	protected String location="chennai";
+	protected String address="abc west car street";
+	public double amount=20000;
+	public void setAccno(int n) {
+		accno=n;
+	}
+	public int getAccno() {
+		return accno;
+	}
+	public void setIfsc(String s) {
+		ifsccode=s;
+	}
+	public String getIfsc() {
+		return ifsccode;
+	}
+	protected void info() {
+		System.out.println("Main Package");
+	}
+}
